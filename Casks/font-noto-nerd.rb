3179b35a11811a1cf7086ae6e19df0bbe8a311b3
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2023-08-03 00:24:48 +0800

    feat: bump `font-noto-nerd`

--- Casks/font-noto-nerd.rb
+++ Casks/font-noto-nerd.rb
@@ -1,19 +1,34 @@
-# Copy from
-# https://github.com/Homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-nerd-font.rb
+# Copy from https://github.com/Homebrew/homebrew-cask-fonts/blob/master/Casks/font-noto-nerd-font.rb
+# we don't need to install all fonts, so we just copy the font we need
+#
+# NOTE:
+# `Noto` = Noto is a font family that Commissioned by Google under the Apache License 2.0
+# `Noto Sans` = Noto Sans is a sans-serif (無襯線) font family
+# `Mono` = Monospaced Fonts (All same width - good for developers)
+# `Propo` = Proportional Fonts (GUI elements or edit a presentation etc)
+# `Nerd` = Nerd Fonts patches developer targeted fonts with a high number of glyphs (icons)
 
 cask "font-noto-nerd" do
-  version "2.1.0"
-  sha256 "1f90f44ff557dd0d8f2ac50822c1ee53aa898a907e6d308e96913a479c65beca"
+  version "3.0.2"
+  sha256 "075927e1725c93efd9ba230499d98291559f45356b0b56b1e545ec3eda946a6f"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Noto.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "Noto Nerd Font families (Noto)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font "Noto Emoji Nerd Font Complete Mono.ttf"
-  font "Noto Sans Mono ExtraLight Nerd Font Complete.ttf"
-  font "Noto Sans Mono Light Nerd Font Complete.ttf"
-  font "Noto Sans Mono Medium Nerd Font Complete.ttf"
-  font "Noto Sans Mono Regular Nerd Font Complete.ttf"
-  font "Noto Sans Mono SemiBold Nerd Font Complete.ttf"
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "NotoSansMNerdFontMono-Black.ttf"
+  font "NotoSansMNerdFontMono-Bold.ttf"
+  font "NotoSansMNerdFontMono-ExtraBold.ttf"
+  font "NotoSansMNerdFontMono-ExtraLight.ttf"
+  font "NotoSansMNerdFontMono-Light.ttf"
+  font "NotoSansMNerdFontMono-Medium.ttf"
+  font "NotoSansMNerdFontMono-Regular.ttf"
+  font "NotoSansMNerdFontMono-SemiBold.ttf"
+  font "NotoSansMNerdFontMono-Thin.ttf"
 end
